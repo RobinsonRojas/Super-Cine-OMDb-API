18ebf512a3828496b4e5437342c888f06b29a68b
Robinson Arley Rojas Novoa <totorox123@gmail.com>
2021-05-27 02:42:05 -0500

    Revert "ver 1.12"
    This reverts commit d0ee346fd03375666f87a73a72d451ebbb3f405e.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-import List from "./containers/List";
+import List  from "./containers/List";
 
-import 'bootswatch/dist/lux/bootstrap.min.css';
+import 'bootswatch/dist/lux/bootstrap.min.css'; 
 
 const App = () => {
     return (
-        <Fragment>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/" >Super Cine</a>
-
-                </div>
-            </nav>
-            <main className="bg-dark">
-                <div className="container">
-                    <List />
-                </div>
-            </main>
-        </Fragment>
+        <main className= "bg-dark">
+            <div className="container">
+                <List/>
+            </div>
+        </main>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
