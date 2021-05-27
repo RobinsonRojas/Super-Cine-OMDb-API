a2ae1f1369066ff7a2b4d5b32269e0a02082de40
Robinson Arley Rojas Novoa <totorox123@gmail.com>
2021-05-27 02:57:40 -0500

    pasado

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,31 +1,71 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Card from '../components/Card/Card'
+
+const API = 'http://www.omdbapi.com/?i=tt3896198&apikey=42d1da59'
 
 class List extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            data: []
+            data: [],
+            searchTerm: '',
+            error: ''
         }
     }
 
     async componentDidMount() {
-        const res = await fetch('../../assets/data.json')
+        //const res = await fetch('../../assets/data.json')
+        const res = await fetch(`${API}&s=batman`)
         const resJSON = await res.json()
-        this.setState({ data: resJSON })
+        console.log(resJSON)
+        this.setState({ data: resJSON.Search })
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault()
+        if (!this.state.searchTerm) {
+            return this.setState({ error: 'Por favor colocar un texto valido' })
+        }
+
+        const res = await fetch(`${API}&s=${this.state.searchTerm}`)
+        const data = await res.json();
+
+        if (!data.Search) {
+            return this.setState({ error: 'No hay resultados' })
+        }
+        this.setState({ data: data.Search, error: '', searchTerm: '' })
     }
 
     render() {
         return (
-            <div className="row">
-                {
-                    this.state.data.map(movie => {
-                        return <Card movie={movie} />
-                    })
-                }
-            </div>
+            <Fragment>
+                <div className="row">
+                    <div className="col-md-4 offset-md-4 p-4">
+                        <form onSubmit={(e) => this.handleSubmit(e)}>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Buscar"
+                                onChange={e => this.setState({ searchTerm: e.target.value })}
+                                autoFocus
+                                value={this.state.searchTerm}
+                            />
+                        </form>
+                        <p className="text-white">
+                            {this.state.error ? this.state.error : ''}
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    {
+                        this.state.data.map((movie, i) => {
+                            return <Card movie={movie} key={i} />
+                        })
+                    }
+                </div>
+            </Fragment>
         )
     }
 }
